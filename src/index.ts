738f570e043e97fd173d047d11c5f46f3ba7dfe4
Felix Pasillas <pasillas.felix@yahoo.com>
2026-08-11 19:32:02 -0700

    Add health check route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.get('/health', (req, res): void => {
+  res.status(200).json({
+    success: true,
+    data: { status: 'ok' },
+    error: null,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
