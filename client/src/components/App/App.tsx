81a754380f12841940102b39df61bc235368a0b4
Felix Pasillas <pasillas.felix@yahoo.com>
2026-09-10 20:55:32 -0700

    Set up React Router with routes for Intro, KnowledgeBase, and Chat

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,11 +1,19 @@
-import "./App.css"
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import AppLayout from "../AppLayout/AppLayout";
+import Intro from "../../pages/Intro/Intro";
 import KnowledgeBase from "../../pages/KnowledgeBase/KnowledgeBase";
+import Chat from "../../pages/Chat/Chat";
 
 function App() {
   return (
-    <div className="app">
-      <KnowledgeBase />
-    </div>
+    <Routes>
+      <Route path="/" element={<Intro />} />
+      <Route element={<AppLayout />}>
+        <Route path="/knowledge" element={<KnowledgeBase />} />
+        <Route path="/chat" element={<Chat />} />
+      </Route>
+    </Routes>
   );
 }
 
