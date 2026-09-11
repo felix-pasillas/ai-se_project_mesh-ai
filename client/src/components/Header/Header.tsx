ed202137b1c3d14c04f5471f8556c99f1654cb33
Felix Pasillas <pasillas.felix@yahoo.com>
2026-09-10 21:33:35 -0700

    Build Header component with logo, NavLink navigation

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,5 +1,25 @@
+import { NavLink } from "react-router-dom";
 import "./Header.css";
+import logo from "../../assets/logo.png";
 
 export default function Header() {
-  return <header className="header">Header</header>;
+  function getNavLinkClass({ isActive }: { isActive: boolean }) {
+    return isActive
+      ? "header__link header__link--active"
+      : "header__link";
+  }
+
+  return (
+    <header className="header">
+      <img src={logo} alt="Mesh AI" className="header__logo" />
+      <nav className="header__nav">
+        <NavLink to="/knowledge" className={getNavLinkClass}>
+          Knowledge Base
+        </NavLink>
+        <NavLink to="/chat" className={getNavLinkClass}>
+          Chat
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
